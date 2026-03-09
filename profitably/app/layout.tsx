b9f9f6974ca39fdd3831ac29b7818dba3488d54a
calvin1011 <calvinssendawula@gmail.com>
2026-03-09 10:10:16 -0500

    Implement Phase 0 of the Rekodry UI animations

--- profitably/app/layout.tsx
+++ profitably/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import './globals.css'
 import React from "react";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en" className="dark" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <Toaster position="bottom-right" richColors />
       </body>
     </html>
   )
